be9a96bfbd38f2cacf9756cf6ee60b20d2fb68f3
Tatyana kudryavtseva <Tatyana@tatyanas-macbook-pro.local>
2016-07-23 19:57:23 +0200

    Initial Commit

---
+++ Word FlakesTests/Word_FlakesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Word_FlakesTests.swift
+//  Word FlakesTests
+//
+//  Created by Tatyana kudryavtseva on 23/07/16.
+//  Copyright © 2016 Organized Chaos. All rights reserved.
+//
+
+import XCTest
+@testable import Word_Flakes
+
+class Word_FlakesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
